883bc05aef88b50e940cd306173543d726b7f345
lei cui <allen5205189@gmail.com>
2017-02-12 15:38:36 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit ,:update , :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -9,19 +10,11 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "您没有权限。"
-    end
+    find_group_and_check_permission
   end
 
   def destroy
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "您没有权限。"
-    end
+    find_group_and_check_permission
 
     @group.destroy
     flash[:alert] = "Group deleted"
@@ -29,11 +22,7 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "您没有权限。"
-    end
+    find_group_and_check_permission
 
     if @group.update (group_params)
     redirect_to groups_path, notice: "Update Success"
@@ -60,6 +49,12 @@ class GroupsController < ApplicationController
 
   private
 
+def find_group_and_check_permission
+  @group = Group.find(params[:id])
+  if current_user != @group.user
+    redirect_to root_path, alert: "对不起，你没有权限。"
+  end
+end
   def group_params
     params.require(:group).permit(:title,:description)
   end
